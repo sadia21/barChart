feb343ffda35432881245726ea91eeb444ea7db5
sadia21 <61055917+sadia21@users.noreply.github.com>
2021-10-10 14:01:50 +0200

    Initial commit

---
+++ stackedBarChart.js
@@ -0,0 +1,124 @@
+var downTimeData = [];
+console.log("hain g");
+var machineData = [
+  {
+    startTime: "10:30",
+    endTime: "10:50",
+    scanType: "Head Scan",
+    timeType: "Usage Time",
+  },
+  {
+    startTime: "11:15",
+    endTime: "11:30",
+    scanType: "Leg Scan",
+    timeType: "Usage Time",
+  },
+  {
+    startTime: "11:45",
+    endTime: "12:10",
+    scanType: "Spine Scan",
+    timeType: "Usage Time",
+  },
+  {
+    startTime: "12:15",
+    endTime: "12:30",
+    scanType: " ",
+    timeType: "Down Time",
+  },
+];
+var object = machineData; // = JSON.parse(JSON.stringify(machineData));
+
+for (var scan in object) {
+  if (object[scan].timeType === "Usage Time") {
+    console.log("This is a downtime ");
+    var hours;
+    hours = object[scan].startTime.split(":");
+    console.log(hours);
+    downTimeData.push("x: " + hours[0] + "," + "y: " + object[scan].endTime);
+  }
+  console.log(downTimeData);
+}
+
+var delayed;
+new Chart(document.getElementById("stackedbar-chart"), {
+  type: "bar",
+  data: {
+    datasets: [
+      {
+        label: "Machine in use",
+        backgroundColor: "lightgreen",
+        borderColor: "lightgreen",
+        data: [
+          {
+            x: "7:00",
+            y: [50, 60],
+          },
+          {
+            x: "7:00",
+            y: [20, 40],
+          },
+          {
+            x: "8:00",
+            y: [8, 21],
+          },
+          {
+            x: "8:00",
+            y: [34, 41],
+          },
+          {
+            x: "8:00",
+            y: [47, 57],
+          },
+          {
+            x: "9:00",
+            y: [50, 60],
+          },
+          {
+            x: "10:00",
+            y: [30, 43],
+          },
+        ],
+      },
+      {
+        label: "Down time",
+        backgroundColor: "red",
+        borderColor: "red",
+        data: [
+          {
+            x: "10:00",
+            y: [10, 18],
+          },
+          {
+            x: "9:00",
+            y: [20, 50],
+          },
+        ],
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: "Predicted performance utilization chart of Machines",
+    },
+
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === "data" && context.mode === "default" && !delayed) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    x: {
+      stacked: true,
+    },
+
+    y: {},
+  },
+});
