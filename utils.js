8b9447ecd758cc937958b49faac19304762f2c7d
sadia21 <61055917+sadia21@users.noreply.github.com>
2021-10-27 13:13:53 +0200

    updated the utils function and set return type for getTotalpercentageofscanData function

--- utils.js
+++ utils.js
@@ -7,5 +7,18 @@ function getThehours(spliteddata) {
 function getTheminutes(spliteddata) {
   return spliteddata[1];
 }
+var totalpercentageData = 0;
+function getTotalpercentageOfscanData(labels, percentageData) {
+  for (var counter in labels) {
+    if (labels[counter] != "Prep Time") {
+      if (labels[counter] != "Down") {
+        if (labels[counter] != "Free Machine") {
+          totalpercentageData = totalpercentageData + percentageData[counter];
+        }
+      }
+    }
+  }
+  return totalpercentageData;
+}
 
-export { splitTime, getThehours, getTheminutes };
+export { splitTime, getThehours, getTheminutes, getTotalpercentageOfscanData };
